f829218e3da893583eee722898607daeaef8e951
kduyyyy1levinci <122582194+kduyyyy1levinci@users.noreply.github.com>
2024-10-09 20:44:39 +0700

    Update footer

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,5 +1,5 @@
 const Footer = () => {
-  return <footer className="text-center mt-auto p-4 bg-blue-400">footervippro0123</footer>;
+  return <footer className="text-center mt-auto p-4 font-bold">Copy © 2024 The TESFU BOYS</footer>;
 };
 
 export default Footer;
